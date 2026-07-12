9f5726b5aab1c440fe23e8ef16c1a4ff8a01b52c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "cosmosdb_mongo_user_definitions" {
-  description = "All cosmosdb_mongo_user_definition resources"
-  value       = azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions
-  sensitive   = true
-}
 output "cosmosdb_mongo_user_definitions_cosmos_mongo_database_id" {
-  description = "List of cosmos_mongo_database_id values across all cosmosdb_mongo_user_definitions"
-  value       = [for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : v.cosmos_mongo_database_id]
+  description = "Map of cosmos_mongo_database_id values across all cosmosdb_mongo_user_definitions, keyed the same as var.cosmosdb_mongo_user_definitions"
+  value       = { for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : k => v.cosmos_mongo_database_id }
 }
 output "cosmosdb_mongo_user_definitions_inherited_role_names" {
-  description = "List of inherited_role_names values across all cosmosdb_mongo_user_definitions"
-  value       = [for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : v.inherited_role_names]
+  description = "Map of inherited_role_names values across all cosmosdb_mongo_user_definitions, keyed the same as var.cosmosdb_mongo_user_definitions"
+  value       = { for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : k => v.inherited_role_names }
 }
 output "cosmosdb_mongo_user_definitions_password" {
-  description = "List of password values across all cosmosdb_mongo_user_definitions"
-  value       = [for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : v.password]
+  description = "Map of password values across all cosmosdb_mongo_user_definitions, keyed the same as var.cosmosdb_mongo_user_definitions"
+  value       = { for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : k => v.password }
   sensitive   = true
 }
 output "cosmosdb_mongo_user_definitions_username" {
-  description = "List of username values across all cosmosdb_mongo_user_definitions"
-  value       = [for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : v.username]
+  description = "Map of username values across all cosmosdb_mongo_user_definitions, keyed the same as var.cosmosdb_mongo_user_definitions"
+  value       = { for k, v in azurerm_cosmosdb_mongo_user_definition.cosmosdb_mongo_user_definitions : k => v.username }
 }
 
