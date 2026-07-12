74ad0edf3aae909a86d5c74f23f879cf06f65d31
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:03 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.cosmosdb_mongo_user_definitions : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_cosmosdb_mongo_user_definition" "cosmosdb_mongo_user_definitions" {
   for_each = var.cosmosdb_mongo_user_definitions
 
   cosmos_mongo_database_id = each.value.cosmos_mongo_database_id
-  password                 = each.value.password
+  password                 = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   username                 = each.value.username
   inherited_role_names     = each.value.inherited_role_names
 }
--- variables.tf
+++ variables.tf
@@ -4,16 +4,20 @@ Map of cosmosdb_mongo_user_definitions, attributes below
 Required:
     - cosmos_mongo_database_id
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - username
 Optional:
     - inherited_role_names
 EOT
 
   type = map(object({
-    cosmos_mongo_database_id = string
-    password                 = string
-    username                 = string
-    inherited_role_names     = optional(list(string))
+    cosmos_mongo_database_id       = string
+    password                       = string
+    password_key_vault_id          = optional(string)
+    password_key_vault_secret_name = optional(string)
+    username                       = string
+    inherited_role_names           = optional(list(string))
   }))
 }
 
